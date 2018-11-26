aad5ec7df43fbe7284483d4048cda5489604b3b5
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2018-11-26 13:43:03 -0500

    add story to replace d3 when first time

--- FrontEnd/src/views/Dashboard/Dashboard.jsx
+++ FrontEnd/src/views/Dashboard/Dashboard.jsx
@@ -7,101 +7,6 @@ import LeafMap from "components/Map/Map";
 import D3_1 from '../../d3_components/D3_1';
 import D3_2 from '../../d3_components/D3_2';
 
-
-// Info of different years
-/*
- * Info of different countries
-  
-
-const worldStats = {
-  "population": "7.7B",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-const brazilStats = {
-  "population": "100M",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-
-const colombiaStats = {
-  "population": "100M",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-
-const mexicoStats = {
-  "population": "100M",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-const nepalStats = {
-  "population": "100M",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-const pakistanStats = {
-  "population": "100M",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-const polandStats = {
-  "population": "100M",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-const nigeriaStats = {
-  "population": "100M",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-const costaricaStats = {
-  "population": "100M",
-  "revenue": "0.5M/ppl",
-  "twitters": "1M/ppl",
-};
-*/
-
-// Stats Card Object
-const statsCardPopulation = {
-  "name": "Population",
-  "value": "10M",
-  "icon": "pe-7s-id text-warning",
-  "statsIcon": "fa fa-refresh",
-  "statsText": "3 minutes ago",
-};
-
-const statsCardRevenue = {
-  "name": "Revenue",
-  "value": "10M",
-  "icon": "pe-7s-cash text-success",
-  "statsIcon": "fa fa-refresh",
-  "statsText": "3 minutes ago",
-};
-
-const statsCardHappiness = {
-  "name": "Happiness",
-  "value": "5",
-  "icon": "pe-7s-smile text-danger",
-  "statsIcon": "fa fa-refresh",
-  "statsText": "3 minutes ago",
-};
-
-const statsCardTwitters = {
-  "name": "Twitters",
-  "value": "100M",
-  "icon": "fa fa-twitter text-info",
-  "statsIcon": "fa fa-refresh",
-  "statsText": "3 minutes ago",
-};
-
-const statsCardObjs = [
-  statsCardPopulation,
-  statsCardRevenue,
-  statsCardHappiness,
-  statsCardTwitters,
-];
-
 const targetCountries = ["Fly Back", "Brazil","Colombia", "Costa Rica", "Pakistan", "Mexico", "Poland", "Nigeria"];
 const countryMapper = {"Brazil" : "brazil",
                        "Colombia" : "colombia",
@@ -110,6 +15,7 @@ const countryMapper = {"Brazil" : "brazil",
                        "Mexico" : "mexico",
                        "Poland" : "poland",
                        "Nigeria" : "nigeria"};
+
 
 class Dashboard extends Component {
 
@@ -120,8 +26,8 @@ class Dashboard extends Component {
       countriesJson: {},
       citiesJson: {},
       countries: [],
-      statsCardObjs: statsCardObjs,
       selectedCountry: "Fly Back",
+      first:true,
     };
 
     this.handleButtonClick = this.handleButtonClick.bind(this)
@@ -165,57 +71,50 @@ class Dashboard extends Component {
 
 
   handleButtonClick(country){
-    statsCardPopulation.name = country;
-    statsCardPopulation.value = null;
-    this.setState({selectedCountry:country});
+    this.setState({selectedCountry:country, first:false});
     
   }
 
   render() {
-    const statsCards = statsCardObjs.map((obj) => {
-      return (
-        <Col key={obj.name} lg={3} sm={6}>
-          <StatsCard
-            bigIcon={<i className={obj.icon} />}
-            statsText={obj.name}
-            statsValue={obj.value}
-            statsIcon={<i className={obj.statsIcon} />}
-            statsIconText={obj.statsText}
-          />
-        </Col>);
-    });
-
     if(!this.state.isLoaded){
         return <ProgressBar now={100}/>;
     }
+    let d3_1 = <D3_1
+      title="D3_1"
+      countriesJson={this.state.countriesJson}
+      citiesJson={this.state.citiesJson}
+      countries={this.state.countries} 
+      targetCountries ={targetCountries}
+      selectedCountry={this.state.selectedCountry}
+      /> ;
+    let d3_2 = <D3_2
+      title="D3_2"
+      countriesJson={this.state.countriesJson}
+      citiesJson={this.state.citiesJson}
+      countries={this.state.countries} 
+      targetCountries ={targetCountries}
+      selectedCountry={this.state.selectedCountry}
+    /> ;
 
     return (
       <div>
         <div className="content">
           <Grid fluid>
             <Row>
-              {/*{statsCards}*/}
-            </Row>
-            <Row>
               <Col md={6}>
-                  <D3_1
-                    title="D3_1"
-                    data={[5, 10, 1, 3]}
-                    countriesJson={this.state.countriesJson}
-                    citiesJson={this.state.citiesJson}
-                    countries={this.state.countries} 
-                    targetCountries ={targetCountries}
-                    selectedCountry={this.state.selectedCountry}
-                  />
-                  <D3_2
-                    title="D3_2"
-                    data={[5, 10, 1, 3]}
-                    countriesJson={this.state.countriesJson}
-                    citiesJson={this.state.citiesJson}
-                    countries={this.state.countries} 
-                    targetCountries ={targetCountries}
-                    selectedCountry={this.state.selectedCountry}
-                  />
+              {this.state.first ? <div>
+                  <h1>Migration and HDI</h1>
+                  <p> At the end of 2016, the total number of refugees and asylum seekers in the world hit 25.9 million people.<br />
+                  There are more than 244 million international migrants, globally. <br/> 
+                  Migration depends on a lot of factors, from war and poverty to educational attainment. <br/>
+                  Now we've told the story, so let's look at the numbers. <br/> 
+                  </p>
+                </div> :
+                <div>
+                  {d3_1}
+                  {d3_2}
+                </div>
+                }
               </Col> 
               <Col md={6}>
                 <LeafMap 
